399a0ed32fc148cfacf5adca0462a57441086acf
Fathurrahman <fathur.ashter15@gmail.com>
2022-09-14 15:21:14 +0800

    fix bug

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
+const express = require('express');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
