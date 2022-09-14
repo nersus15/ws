ce8e74910e05aa072e93c74824b0dce47bd5742e
Fathurrahman <fathur.ashter15@gmail.com>
2022-09-14 15:31:58 +0800

    fix bug

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ const express = require('express');
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const { Server } = require('ws');
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
